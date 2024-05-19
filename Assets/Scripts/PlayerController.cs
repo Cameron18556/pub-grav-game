a3675b8d98947994b74f22c88f43483bf2cce72c
CameronAIE <s221308@students.aie.edu.au>
2024-05-19 22:42:32 +0930

    i wasa gonna start the gravity part but forgot i i didnt add looking so thats added here

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 inputMovementVector3;
     private Rigidbody rb;
     public float movementSpeed;
+    public float lookSensitivity;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +19,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += inputMovementVector3 * movementSpeed * Time.deltaTime;
+        
+        transform.position += transform.forward * inputMovementVector3.z * movementSpeed * Time.deltaTime;
+        transform.position += transform.right * inputMovementVector3.x * movementSpeed * Time.deltaTime;
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         inputMovementVector = context.ReadValue<Vector2>();
         inputMovementVector3 = new Vector3(inputMovementVector.x, 0, inputMovementVector.y);
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        Vector2 input = context.ReadValue<Vector2>();
+        
+        input *= Time.deltaTime;
+        input *= lookSensitivity;
+        if(input.y < -90)
+        {
+            input.y = -90;
+        }
+        if (input.y > 90)
+        {
+            input.y = 90;
+        }
+
+        transform.localEulerAngles += new Vector3(input.y, -input.x, 0);
     }
 }
